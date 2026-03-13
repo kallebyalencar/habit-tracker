78af065c3ff9f083b9a6016860118f597377d426
Kalleby Alencar <alencar.jkalleby@gmail.com>
2026-03-12 22:26:28 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+  <p class="text-center">
+    Criado por Kalleby Alencar. O código fonte está no <a href="https://github.com/kallebyalencar/habit-tracker " class="underline">GitHub.</a>
+  </p>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        {{ config('app.name') }}
-    </title>
-    @vite('resources/css/app.css') <!-- O caminho do css que criamos -->
-</head>
-<body>
-    <h1 class="text-3xl font-bold underline text-red-500">Bem-vindo ao Meu App</h1>
-</body>
-</html>
+<x-layout>
+    
+    <main class="py-10">
+        <h1>
+            Veja seus hábitos ganharem vida
+        </h1>
+    </main>
+
+</x-layout>
