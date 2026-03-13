2290ad00296d3ededd4b2b0e5796c32f1b0f6ba5
Kalleby Alencar <alencar.jkalleby@gmail.com>
2026-03-12 22:06:14 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,14 @@
-<h1>Bem-vindo ao Meu App</h1>
-<p>Olá, {{$name}}!</p>
-<p>Seus hábitos são:</p>
-<ul>
-    @foreach($habits as $habit)
-        <li>
-            {{$habit}}
-        </li>
-    @endforeach
-</ul>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        {{ config('app.name') }}
+    </title>
+    @vite('resources/css/app.css') <!-- O caminho do css que criamos -->
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-red-500">Bem-vindo ao Meu App</h1>
+</body>
+</html>
