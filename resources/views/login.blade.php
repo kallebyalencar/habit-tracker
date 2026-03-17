9e3875d2d23d87566b286c8250747bd5c9bed7ef
Kalleby Alencar <alencar.jkalleby@gmail.com>
2026-03-16 22:59:47 -0300

    feat(Auth): add login functionality and user session

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    
+    <main class="py-10">
+        <h1>
+            Faça Login para acessar o Habit Tracker
+        </h1>
+    </main>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use Illuminate\Container\Attributes\Auth;
 
-
+// SITE
 Route::get('/', [SiteController::class, 'index']);
+
+// Login
+Route::get('/login', [LoginController::class, 'index']); 
